12b07bde2207d5536f9382a1e6a90ffdb6bccf1f
fatmakusmez <fatma.kusmez@motopp.nl>
2024-08-18 18:08:26 +0200

    getting codes

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\AnasShehadeh\Desktop\E-Shop\env
+set VIRTUAL_ENV=C:\Users\kusme\Desktop\MERVE\Motopp\W7_8_9_10\marktplaats_project\oranje_shop\env
 
 if not defined PROMPT set PROMPT=$P$G
 
